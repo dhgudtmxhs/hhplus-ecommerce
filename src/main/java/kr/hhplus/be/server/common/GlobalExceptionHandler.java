50779eff22d4fa26c80596576f3401cc12a18100
ohhyoungseok <gudtjr1350@gmail.com>
2025-01-10 07:54:15 +0900

    common 설정

---
+++ src/main/java/kr/hhplus/be/server/common/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package kr.hhplus.be.server.common;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        String errorMessage = ex.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        ErrorResponse response = new ErrorResponse(ErrorCode.INVALID_INPUT.getCode(), errorMessage);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+
+        String errorMessage = ex.getConstraintViolations().iterator().next().getMessage();
+
+        ErrorResponse response = new ErrorResponse(ErrorCode.INVALID_INPUT.getCode(), errorMessage);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        // 로깅 및 기타 처리
+        ErrorResponse response = new ErrorResponse("INTERNAL_SERVER_ERROR", "예상치 못한 오류가 발생했습니다.");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
