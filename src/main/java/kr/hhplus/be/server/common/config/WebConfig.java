fc9d2c4dc3792ec2c8865d9556a6d28c65d6e0e8
ohhyoungseok <gudtjr1350@gmail.com>
2025-01-13 22:24:00 +0900

    feat: 요청과 응답 로깅 기능을 위한 LoggingInterceptor 생성, interceptor 적용 설정을 위한 WebConfig 추가
    - 요청 : method, uri
    - 응답 : status, processing time

---
+++ src/main/java/kr/hhplus/be/server/common/config/WebConfig.java
@@ -0,0 +1,22 @@
+package kr.hhplus.be.server.common.config;
+
+import kr.hhplus.be.server.common.intercepter.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**"); // 모든 경로에 인터셉터 적용
+    }
+}
---
+++ src/main/java/kr/hhplus/be/server/common/intercepter/LoggingInterceptor.java
@@ -0,0 +1,29 @@
+package kr.hhplus.be.server.common.intercepter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+
+        log.info("요청 시작 - METHOD: {}, URI: {}", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        long startTime = (long) request.getAttribute("startTime");
+        long duration = System.currentTimeMillis() - startTime;
+
+        log.info("요청 종료 - STATUS: {}, PROCESSING TIME: {}ms", response.getStatus(), duration);
+    }
+}
