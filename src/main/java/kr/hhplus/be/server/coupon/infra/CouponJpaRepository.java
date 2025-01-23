67714fa6b2b6906c4acadc12b2dd41ea774e424a
Oh Hyoung Seok <126877943+dhgudtmxhs@users.noreply.github.com>
2025-01-23 17:00:18 +0900

    refactor: 쿠폰 발급 - 비관적 락 방식을 유지하면서 QueryHint로 잠재적 데드락 방지 로직 추가

--- src/main/java/kr/hhplus/be/server/coupon/infra/CouponJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/coupon/infra/CouponJpaRepository.java
@@ -1,17 +1,20 @@
 package kr.hhplus.be.server.coupon.infra;
 
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import kr.hhplus.be.server.coupon.domain.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
 
-    @Query("SELECT c FROM Coupon c WHERE c.id = :couponId")
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "1000")})
+    @Query("SELECT c FROM Coupon c WHERE c.id = :couponId")
     Optional<Coupon> findByIdForUpdate(@Param("couponId") Long couponId);
 }
