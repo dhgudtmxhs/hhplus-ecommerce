80e7d271768f72c0b77597401813de51186005f9
Oh Hyoung Seok <126877943+dhgudtmxhs@users.noreply.github.com>
2025-01-23 19:04:24 +0900

    refactor: 주문 - 한 사용자의 동일 주문 동시 요청 방지를 위한 Redis 락 적용
    - 동일 사용자가 동일 주문을 동시에 여러 번 요청하는 상황 방지
    - 상품 재고 동시성 제어는 아직 비관적 락으로 제어
    - 추후 분산 환경을 고려해 Redis 기반 분산 락으로 적용함

--- src/main/java/kr/hhplus/be/server/order/application/OrderFacade.java
+++ src/main/java/kr/hhplus/be/server/order/application/OrderFacade.java
@@ -6,11 +6,14 @@ import kr.hhplus.be.server.product.domain.ProductService;
 import kr.hhplus.be.server.user.domain.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,31 +26,50 @@ public class OrderFacade {
     private final ProductService productService;
     private final OrderItemDataMapper orderItemDataMapper;
     private final OrderInfoMapper orderInfoMapper;
+    private final RedissonClient redissonClient;
 
     @Transactional
     public OrderInfo createOrder(OrderCommand command) {
+        String lockKey = "lock:user:" + command.userId();
+        RLock lock = redissonClient.getLock(lockKey);
 
-        userService.getUser(command.userId());
+        boolean isLocked = false;
+        try {
 
-        Map<Long, Long> productQuantities = command.productOrderCommands().stream()
-                .collect(Collectors.toMap(ProductOrderCommand::productId, ProductOrderCommand::quantity));
+            isLocked = lock.tryLock(0, 5, TimeUnit.SECONDS);
+            if (!isLocked) {
+                throw new IllegalStateException("동일 사용자는 동시에 여러 주문을 할 수 없습니다.");
+            }
 
-        List<Long> productIds = List.copyOf(productQuantities.keySet());
+            userService.getUser(command.userId());
 
-        List<OrderItemData> orderItemDataList = productService.findByIdForUpdate(productIds).stream()
-                .map(product -> orderItemDataMapper.toOrderItemData(
-                        product,
-                        productQuantities.get(product.getId())
-                ))
-                .collect(Collectors.toList());
+            Map<Long, Long> productQuantities = command.productOrderCommands().stream()
+                    .collect(Collectors.toMap(ProductOrderCommand::productId, ProductOrderCommand::quantity));
 
-        productService.deductStock(productQuantities);
+            List<Long> productIds = List.copyOf(productQuantities.keySet());
 
-        Order order = orderService.createOrder(command.userId(), orderItemDataList);
+            List<OrderItemData> orderItemDataList = productService.findByIdForUpdate(productIds).stream()
+                    .map(product -> orderItemDataMapper.toOrderItemData(
+                            product,
+                            productQuantities.get(product.getId())
+                    ))
+                    .collect(Collectors.toList());
 
-        List<OrderItemInfo> orderItemInfos = orderItemDataList.stream()
-                .map(orderInfoMapper::toOrderItemInfo)
-                .collect(Collectors.toList());
-        return orderInfoMapper.toOrderInfo(order, orderItemInfos);
+            productService.deductStock(productQuantities);
+
+            Order order = orderService.createOrder(command.userId(), orderItemDataList);
+
+            List<OrderItemInfo> orderItemInfos = orderItemDataList.stream()
+                    .map(orderInfoMapper::toOrderItemInfo)
+                    .collect(Collectors.toList());
+            return orderInfoMapper.toOrderInfo(order, orderItemInfos);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("주문 처리 중 interruptr 발생", e);
+        } finally {
+            if (isLocked && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
     }
 }
