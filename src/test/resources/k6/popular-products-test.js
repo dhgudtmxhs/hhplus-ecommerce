7a827851d36bb8339b218f4ce0719de4a897160a
Oh Hyoung Seok <126877943+dhgudtmxhs@users.noreply.github.com>
2025-02-05 17:22:36 +0900

    test: 인기 상품 API 부하 테스트 스크립트 수정

--- src/test/resources/k6/popular-products-test.js
+++ src/test/resources/k6/popular-products-test.js
@@ -3,12 +3,12 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '10s', target: 200 },  // 10초 동안 200명 부하 증가
+        { duration: '10s', target: 100 },  // 10초 동안 100명 부하 증가
         { duration: '10s', target: 500 },  // 10초 동안 500명까지 증가
         { duration: '10s', target: 800 },  // 10초 동안 800명까지 증가
         { duration: '40s', target: 2000 }, // 40초 동안 2000명 유지
-        { duration: '10s', target: 300 },  // 10초 동안 300명으로 감소
-        { duration: '5s', target: 0 }      // 5초 동안 부하 종료
+        { duration: '10s', target: 100 },  // 10초 동안 100명으로 감소
+        { duration: '5s', target: 0 }     // 5초 동안 부하 종료
     ],
 };
 
@@ -20,5 +20,5 @@ export default function () {
         'response time < 2s': (r) => r.timings.duration < 2000, // 2초 넘는 응답 확인
     });
 
-    sleep(0.2);  // 요청 간격
+    sleep(0.1);  // 요청 간격
 }
