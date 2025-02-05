0b46a76da0171673950b363e11989ce07107583a
ohhyoungseok <gudtjr1350@gmail.com>
2025-02-05 22:31:57 +0900

    fix: Redis 역직렬화 오류 해결을 위해 BaseEntity의 날짜 필드에 JsonFormat 추가

--- src/main/java/kr/hhplus/be/server/common/entity/BaseEntity.java
+++ src/main/java/kr/hhplus/be/server/common/entity/BaseEntity.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.common.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
 
@@ -13,9 +14,11 @@ public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Column(name = "created_at", updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 
     @PrePersist
