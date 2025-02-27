8db5c5c6927e4f665212d82cc2c17589513cd5dd
Oh Hyoung Seok <126877943+dhgudtmxhs@users.noreply.github.com>
2025-02-27 13:04:02 +0900

    refactor: 포인트 충전 데드락 방지를 위한 QueryHints 사용

--- src/main/java/kr/hhplus/be/server/point/infra/PointJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/point/infra/PointJpaRepository.java
@@ -1,10 +1,12 @@
 package kr.hhplus.be.server.point.infra;
 
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import kr.hhplus.be.server.point.domain.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
@@ -14,5 +16,9 @@ public interface PointJpaRepository extends JpaRepository<Point, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select p from Point p where p.userId = :userId")
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "1000")})
     Optional<Point> findByUserIdForUpdate(@Param("userId") Long userId);
 }
+
+
+
